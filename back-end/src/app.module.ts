125401480a3ecf5b47874fbcd48d4feaadc4ce99
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-02 14:58:29 +0300

    import all modules in the app

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './modules/events/events.module';
+import { UsersModule } from './modules/users/users.module';
+import { ArticlesModule } from './modules/articles/articles.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [EventsModule, UsersModule, ArticlesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
