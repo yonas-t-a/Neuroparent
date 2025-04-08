054a93df2deb184126b57f1d0dcfebe4d89c70e2
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-08 03:44:33 +0300

    setup basic setup

---
+++ back-end/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Backend routes
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+}
+);
