9715360ef634edcb222abae778c39ddb35764846
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-09 14:34:17 +0300

    a controller that get the userEvent by id

--- back-end/controller/userEventController.js
+++ back-end/controller/userEventController.js
@@ -10,3 +10,15 @@ export async function getUserEvent(req,req){
         res.status(500).json({ error: "Error fetching user events" });
     }
 }
+export async function getUserEventById(req,res){
+    const { id } = req.params;
+    try {
+        const userEvent = await userEventModel.getUserEventById(id);
+        if (!userEvent) {
+            return res.status(404).json({ message: "User event not found" });
+        }
+        res.status(200).json(userEvent);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching user event" });
+    }
+}
