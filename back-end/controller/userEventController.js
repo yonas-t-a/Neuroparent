08fad233a7ab2caa44df91106f07872d01919b24
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-09 14:33:39 +0300

    a function that is a controller for the userEvent

--- back-end/controller/userEventController.js
+++ back-end/controller/userEventController.js
@@ -2,3 +2,11 @@
 
 import userEventModel from "../model/userEvent.js";
 
+export async function getUserEvent(req,req){
+    try {
+        const userEvents = await userEventModel.getUserEvent();
+        res.status(200).json(userEvents);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching user events" });
+    }
+}
