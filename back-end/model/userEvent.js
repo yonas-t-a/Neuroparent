ed4d6679246d842ceb0f679a5cf31065d7f5d3bc
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-09 14:27:50 +0300

    create getUserEvent function

--- back-end/model/userEvent.js
+++ back-end/model/userEvent.js
@@ -1,4 +1,9 @@
 
 import { pool } from '../database.js';
-const userEventModel = {};
+const userEventModel = {
+    async getUserEvent() {
+        const [rows] = await pool.query('SELECT * FROM user_event');
+        return rows;
+    },
+};
 export default userEventModel;
