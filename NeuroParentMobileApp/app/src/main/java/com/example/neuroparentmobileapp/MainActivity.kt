11ee6b3c77940922c7bee868aec6957bc08677c2
yonas-t-a <yonas.tessema@aait.edu.et>
2025-04-30 12:49:38 +0300

    clean up

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/MainActivity.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/MainActivity.kt
@@ -16,32 +16,10 @@ import com.example.neuroparentmobileapp.ui.theme.NeuroParentMobileAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             NeuroParentMobileAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NeuroParentMobileAppTheme {
-        Greeting("Android")
     }
 }
