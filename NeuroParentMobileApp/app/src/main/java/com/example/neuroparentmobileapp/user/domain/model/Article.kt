432c1e02d29b79a0a35582a9646858cddc864a05
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 17:46:16 +0300

    article modle

---
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/domain/model/Article.kt
@@ -0,0 +1,9 @@
+package com.example.neuroparentmobileapp.domain.model
+
+data class Article(
+    val id: Int,
+    val title: String,
+    val content: String,
+    val category: String,
+    val imageUrl: String? = null
+)
