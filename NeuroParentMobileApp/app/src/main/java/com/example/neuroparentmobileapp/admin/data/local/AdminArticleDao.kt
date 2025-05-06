21d4115eaa1a55044823efce6fd5f9be47690af6
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 16:07:36 +0300

    add article dao for admin

---
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/data/local/AdminArticleDao.kt
@@ -0,0 +1,22 @@
+package com.example.neuroparentmobileapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.neuroparentmobileapp.domain.model.AdminArticle
+
+@Dao
+interface AdminArticleDao {
+    @Query("SELECT * FROM articles WHERE id = :id")
+    suspend fun getArticleById(id: Int): AdminArticle?
+
+    @Insert
+    suspend fun createArticle(article: AdminArticle)
+
+    @Update
+    suspend fun updateArticle(article: AdminArticle)
+
+    @Query("DELETE FROM articles WHERE id = :id")
+    suspend fun deleteArticle(id: Int)
+}
