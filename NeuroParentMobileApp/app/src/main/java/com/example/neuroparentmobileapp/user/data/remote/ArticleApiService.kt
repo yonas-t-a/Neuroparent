092546f17371829a9019bd53acd448a8a31d084f
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 18:06:36 +0300

    add articleAprServices

---
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/data/remote/ArticleApiService.kt
@@ -0,0 +1,17 @@
+package user.data.remote
+
+import user.data.remote.dto.ArticleDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ArticleApiService {
+
+    @GET("articles")
+    suspend fun getArticles(): List<ArticleDto>
+
+    @GET("articles/{id}")
+    suspend fun getArticleById(@Path("id") id: String): ArticleDto
+
+    @GET("articles/category/{category}")
+    suspend fun getArticlesByCategory(@Path("category") category: String): List<ArticleDto>
+}
