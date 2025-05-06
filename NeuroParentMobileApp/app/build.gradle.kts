2105650318613aa995154b151420bd3d03fc81a0
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 11:32:31 +0300

    add gson dep

--- NeuroParentMobileApp/app/build.gradle.kts
+++ NeuroParentMobileApp/app/build.gradle.kts
@@ -94,4 +94,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
+//    
+    implementation ("com.google.code.gson:gson:2.8.8")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
---
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/core/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.example.neuroparentmobileapp.core.di
+
+import android.content.Context
+import com.example.neuroparentmobileapp.BuildConfig
+import com.example.neuroparentmobileapp.auth.data.remote.AuthApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "http://10.0.2.2:3500/api/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.level = if (BuildConfig.DEBUG)
+            HttpLoggingInterceptor.Level.BODY
+        else
+            HttpLoggingInterceptor.Level.NONE
+
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthApiService(retrofit: Retrofit): AuthApiService {
+        return retrofit.create(AuthApiService::class.java)
+    }
+}
