5e24853679ae8e8f0c3c0eb1fb306e1cc6dc5125
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 10:53:07 +0300

    add id(com.google.dagger.hilt.android) version 2.53.1 apply false  in the main plugin

--- NeuroParentMobileApp/build.gradle.kts
+++ NeuroParentMobileApp/build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+//    Customs
+    id("com.google.dagger.hilt.android") version "2.53.1" apply false
 }
