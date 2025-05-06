29cf2b949a9647d97ebe98b13dc78aeca7d87fbb
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 10:59:19 +0300

    setup Dependency Injection dependencies

--- NeuroParentMobileApp/app/build.gradle.kts
+++ NeuroParentMobileApp/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+//    custom
+    id("kotlin-kapt")
 }
 
 android {
@@ -56,4 +58,13 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+//    custom
+
+    // Dependency Injection
+    implementation("com.google.dagger:hilt-android:2.53.1")
+    kapt("com.google.dagger:hilt-compiler:2.53.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.53.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+
 }
