0fc46f15e8b2bd18c3865ed76366093704d9f645
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 16:51:59 +0300

    more update and delet functionality

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/domain/repository/AdminEventRepository.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/domain/repository/AdminEventRepository.kt
@@ -4,5 +4,6 @@ import com.example.neuroparentmobileapp.domain.model.AdminEvent
 
 interface AdminEventRepository {
     suspend fun createEvent(event: AdminEvent)
-    
+    suspend fun updateEvent(id: Int, event: AdminEvent)
+    suspend fun deleteEvent(id: Int)
 }
