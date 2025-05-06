fddaff000119901f8fadcc998257b85739d0c9df
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-07 00:31:40 +0300

    a list of tips and tricks

---
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/user/presentation/tips/TipsListScreen.kt
@@ -0,0 +1,128 @@
+package com.example.usertiptricks.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.usertiptricks.R
+
+
+
+
+@Composable
+fun ColumnItem(
+    modifier: Modifier,
+    painter: Array<Int>,
+    articleTitle: Array<String>,
+    articleLittleDescription: Array<String>,
+    itemIndex: Int,
+    navController: NavController
+) {
+
+
+    Card(
+        modifier
+            .padding(10.dp)
+            .wrapContentSize()
+            .clickable {
+                navController.navigate(route= "DetailScreen/$itemIndex")
+            },
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            Image(
+                painter = painterResource(id = painter[itemIndex]),
+                contentDescription = articleTitle[itemIndex],
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(90.dp)
+                    .padding(start = 10.dp)
+                    .clip(RoundedCornerShape(12.dp))
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(12.dp)
+            ) {
+                val frontText = if (articleLittleDescription[itemIndex].length > 32)
+                    articleLittleDescription[itemIndex].substring(0, 33) + "..."
+                else
+                    articleLittleDescription[itemIndex] + "..."
+
+                Text(
+                    text = articleTitle[itemIndex],
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = frontText,
+                    fontSize = 16.sp
+                )
+            }
+
+            Icon(
+                painter = painterResource(id = R.drawable.arrow_forward_ios_24),
+                contentDescription = "Go to details",
+                modifier = Modifier
+                    .size(24.dp)
+                    .align(Alignment.CenterVertically)
+            )
+            Spacer(modifier = Modifier.padding(5.dp))
+        }
+    }
+}
+
