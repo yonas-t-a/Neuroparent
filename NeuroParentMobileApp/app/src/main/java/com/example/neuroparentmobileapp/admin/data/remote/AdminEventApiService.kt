49d284420d8a9511864366edb8b21903f15a2118
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-06 16:15:51 +0300

    admin Article api

---
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/data/remote/AdminEventApiService.kt
@@ -0,0 +1,32 @@
+// package com.example.neuroparentmobileapp.admin.data.remote
+
+// import admin.data.remote.dto.EventDto
+// import retrofit2.http.*
+
+// interface AdminEventApiService {
+
+//     @POST("events")
+//     suspend fun createEvent(@Body event: EventDto): EventDto
+
+//     @PUT("events/{id}")
+//     suspend fun updateEvent(@Path("id") id: String, @Body event: EventDto): EventDto
+
+//     @DELETE("events/{id}")
+//     suspend fun deleteEvent(@Path("id") id: String)
+// }
+
+package com.example.neuroparentmobileapp.data.remote
+
+import com.example.neuroparentmobileapp.domain.model.AdminEvent
+import retrofit2.http.*
+
+interface AdminEventApiService {
+    @POST("events")
+    suspend fun createEvent(@Body event: AdminEvent)
+
+    @PUT("events/{id}")
+    suspend fun updateEvent(@Path("id") id: Int, @Body event: AdminEvent)
+
+    @DELETE("events/{id}")
+    suspend fun deleteEvent(@Path("id") id: Int)
+}
