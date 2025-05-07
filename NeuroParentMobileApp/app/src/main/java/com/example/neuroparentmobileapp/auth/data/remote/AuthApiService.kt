7db58007ab9cf1e541e70c79bf560b88afd73ffb
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-07 12:55:12 +0300

    change the type to RegisterRequest

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/data/remote/AuthApiService.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/data/remote/AuthApiService.kt
@@ -1,14 +1,17 @@
 package auth.data.remote
 
-import auth.data.remote.dto.LoginRequest
-import auth.data.remote.dto.LoginResponse
+
+import com.example.neuroparentmobileapp.auth.data.remote.dto.LoginRequest
+import com.example.neuroparentmobileapp.auth.data.remote.dto.LoginResponse
+import com.example.neuroparentmobileapp.auth.data.remote.dto.RegisterRequest
 import retrofit2.http.Body
 import retrofit2.http.POST
+
 
 interface AuthApiService {
     @POST("auth/login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
 
     @POST("auth/register")
-    suspend fun register(@Body request: LoginRequest): LoginResponse
+    suspend fun register(@Body request: RegisterRequest): LoginResponse
 }
