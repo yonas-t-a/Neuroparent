6a5496dbb90a41e19a3793070d1e4b11d9134fc0
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-07 12:52:05 +0300

    change the retrn type to  Resource<AuthUser>

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/domain/repository/AuthRepository.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/domain/repository/AuthRepository.kt
@@ -1,9 +1,10 @@
 package com.example.neuroparentmobileapp.auth.domain.repository
 
 import com.example.neuroparentmobileapp.auth.domain.model.AuthUser
+import com.example.neuroparentmobileapp.auth.data.repository.Resource
 
 interface AuthRepository {
-    suspend fun login(email: String, password: String): AuthUser
-    suspend fun register(name: String, email: String, password: String): AuthUser
-    fun logout()
+    suspend fun login(email: String, password: String): Resource<AuthUser>
+    suspend fun register(name: String, email: String, password: String): Resource<AuthUser>
+    suspend fun logout()
 }
