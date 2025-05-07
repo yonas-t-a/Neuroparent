1ffd160f9c2e055957d522ed9995786188709e3d
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-07 12:50:33 +0300

    update the entitiy def and def PK

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/domain/model/AuthUser.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/domain/model/AuthUser.kt
@@ -1,9 +1,13 @@
 package com.example.neuroparentmobileapp.auth.domain.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user")
 data class AuthUser(
-    val id: Int? = null,          
-    val name: String,            
-    val email: String,           
-    val role: String      
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    val name: String,
+    val email: String,
+    val role: String
 )
 
