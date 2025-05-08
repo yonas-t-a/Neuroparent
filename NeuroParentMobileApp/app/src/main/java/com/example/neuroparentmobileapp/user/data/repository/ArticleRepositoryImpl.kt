759610665dbf41821a2c889cdcea0ce6ec08a460
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-08 11:06:47 +0300

    final user data

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/data/repository/ArticleRepositoryImpl.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/data/repository/ArticleRepositoryImpl.kt
@@ -1,15 +1,12 @@
 package com.example.neuroparentmobileapp.user.data.repository
 
-
 import com.example.neuroparentmobileapp.user.data.remote.ArticleApiService
-import com.example.neuroparentmobileapp.user.data.remote.dto.ArticleDto
+import com.example.neuroparentmobileapp.user.data.remote.dto.toDomain
+import com.example.neuroparentmobileapp.user.domain.model.Article
 import com.example.neuroparentmobileapp.user.domain.repository.ArticleRepository
 
 class ArticleRepositoryImpl(private val apiService: ArticleApiService) : ArticleRepository {
-     override suspend fun getArticles(): List<ArticleDto> {
-         return apiService.getArticles()
+    override suspend fun getArticles(): List<Article> {
+        return apiService.getArticles().map { it.toDomain() }
      }
-//    override suspend fun getArticles(): List<ArticleDto> {
-//        return apiService.getArticles().map { it.toDomain() }
-//    }
 }
--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/data/repository/EventRepositoryImpl.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/user/data/repository/EventRepositoryImpl.kt
@@ -1,22 +1,30 @@
 package com.example.neuroparentmobileapp.user.data.repository
 
-
 import com.example.neuroparentmobileapp.user.data.remote.EventApiService
-import com.example.neuroparentmobileapp.user.data.remote.dto.EventDto
+import com.example.neuroparentmobileapp.user.data.remote.dto.toDomain
+import com.example.neuroparentmobileapp.user.data.remote.dto.UserEventDto
+import com.example.neuroparentmobileapp.user.domain.model.Event
 import com.example.neuroparentmobileapp.user.domain.repository.EventRepository
 
 class EventRepositoryImpl(private val apiService: EventApiService) : EventRepository {
-     override suspend fun getEvents():  List<EventDto> {
-         return apiService.getEvents()
-     }
+    override suspend fun getEvents(): List<Event> {
+        return apiService.getEvents().map { it.toDomain() }
+    }
 
-     override suspend fun registerForEvent(userId: Int, eventId: Int) {
+    override suspend fun registerForEvent(userId: Int, eventId: Int) {
         apiService.registerForEvent(userId, eventId)
     }
 
     override suspend fun cancelRegistration(userId: Int, eventId: Int) {
         apiService.cancelRegistration(userId, eventId)
     }
+
+    override suspend fun getUserEvents(userId: Int): List<Int> {
+        return apiService.getUserEvents()
+            .filter { it.user_id == userId }
+            .map { it.event_id }
+    }
+
     //  Second
 
     //  override suspend fun registerForEvent(id: Int) {
