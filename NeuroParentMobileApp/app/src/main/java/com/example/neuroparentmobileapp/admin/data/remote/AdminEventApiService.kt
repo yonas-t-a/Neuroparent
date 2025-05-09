927ed2e598f5ffebc7a3da26addaf7da5f2a25b9
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-09 23:59:26 +0300

    remove the old v

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/data/remote/AdminEventApiService.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/data/remote/AdminEventApiService.kt
@@ -1,19 +1,4 @@
-// package com.example.neuroparentmobileapp.admin.data.remote
 
-// import admin.data.remote.dto.EventDto
-// import retrofit2.http.*
-
-// interface AdminEventApiService {
-
-//     @POST("events")
-//     suspend fun createEvent(@Body event: EventDto): EventDto
-
-//     @PUT("events/{id}")
-//     suspend fun updateEvent(@Path("id") id: String, @Body event: EventDto): EventDto
-
-//     @DELETE("events/{id}")
-//     suspend fun deleteEvent(@Path("id") id: String)
-// }
 
 package com.example.neuroparentmobileapp.admin.data.remote
 
