a96eb8ee279a5c6adfb31011b9ea38895a140129
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-09 23:58:46 +0300

    remove old version

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/data/remote/AdminArticleApiService.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/admin/data/remote/AdminArticleApiService.kt
@@ -1,34 +1,4 @@
-// package com.example.neuroparentmobileapp.admin.data.remote
 
-// import admin.data.remote.dto.ArticleDto
-// import okhttp3.MultipartBody
-// import okhttp3.RequestBody
-// import retrofit2.http.*
-
-// interface AdminArticleApiService {
-
-//     @Multipart
-//     @POST("articles")
-//     suspend fun createArticle(
-//         @Part("title") title: RequestBody,
-//         @Part("content") content: RequestBody,
-//         @Part("category") category: RequestBody,
-//         @Part img: MultipartBody.Part?
-//     ): ArticleDto
-
-//     @Multipart
-//     @PUT("articles/{id}")
-//     suspend fun updateArticle(
-//         @Path("id") id: String,
-//         @Part("title") title: RequestBody,
-//         @Part("content") content: RequestBody,
-//         @Part("category") category: RequestBody,
-//         @Part img: MultipartBody.Part?
-//     ): ArticleDto
-
-//     @DELETE("articles/{id}")
-//     suspend fun deleteArticle(@Path("id") id: String)
-// }
 package com.example.neuroparentmobileapp.admin.data.remote
 
 import com.example.neuroparentmobileapp.admin.data.remote.dto.AdminArticleDto
