6c30c4d08c89be4b23e0292c9284819c5fe06fca
yonas-t-a <yonas.tessema@aait.edu.et>
2025-05-11 20:40:16 +0300

    update the user respose and token management

--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/data/dto/LoginResponse.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/data/dto/LoginResponse.kt
@@ -1,5 +1,11 @@
 package com.example.neuroparentmobileapp.auth.data.dto
- 
+
 data class LoginResponse(
-    val token: String
-) 
+    val token: String,
+    val user: User
+) {
+    data class User(
+        val id: String,
+        val role: String
+    )
+}
--- NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/data/repository/TokenManager.kt
+++ NeuroParentMobileApp/app/src/main/java/com/example/neuroparentmobileapp/auth/data/repository/TokenManager.kt
@@ -11,12 +11,26 @@ class TokenManager(private val context: Context) {
     companion object {
         private val Context.dataStore by preferencesDataStore("auth_prefs")
         val TOKEN_KEY = stringPreferencesKey("jwt_token")
+        private val USER_ID_KEY = stringPreferencesKey("user_id")
+        private val USER_ROLE_KEY = stringPreferencesKey("user_role")
     }
 
     val token: Flow<String?> = context.dataStore.data.map { it[TOKEN_KEY] }
+    val userId: Flow<String?> = context.dataStore.data.map { it[USER_ID_KEY] }
+    val userRole: Flow<String?> = context.dataStore.data.map { it[USER_ROLE_KEY] }
 
     suspend fun saveToken(token: String) {
         context.dataStore.edit { it[TOKEN_KEY] = token }
+    }
+
+
+
+    suspend fun saveTokenAndUserDetails(token: String, userId: String, role: String) {
+        context.dataStore.edit { preferences ->
+            preferences[TOKEN_KEY] = token
+            preferences[USER_ID_KEY] = userId
+            preferences[USER_ROLE_KEY] = role
+        }
     }
 
     suspend fun clearToken() {
